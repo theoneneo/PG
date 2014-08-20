a2bdae63c4666512246a9b98b8e31c06938783ef
theoneneo <mobi.liubing@gmail.com>
2014-08-20 11:51:33 +0800

    pg

--- PrettyGirl_android/src/com/neo/prettygirl/fragment/PGMainFragment.java
+++ PrettyGirl_android/src/com/neo/prettygirl/fragment/PGMainFragment.java
@@ -1,22 +1,45 @@
 package com.neo.prettygirl.fragment;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.trinea.android.common.entity.FailedReason;
+import cn.trinea.android.common.service.impl.ImageCache;
+import cn.trinea.android.common.service.impl.RemoveTypeLastUsedTimeFirst;
+import cn.trinea.android.common.service.impl.ImageMemoryCache.OnImageCallbackListener;
 
 import com.neo.prettygirl.R;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView.ScaleType;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class PGMainFragment extends BaseFragment {
 	private GridAdapter adapter;
+	public static final ImageCache IMAGE_CACHE = new ImageCache(128, 512);
+	public static final String TAG_CACHE = "image_cache";
+	/** cache folder path which be used when saving images **/
+	public static final String DEFAULT_CACHE_FOLDER = new StringBuilder()
+			.append(Environment.getExternalStorageDirectory().getAbsolutePath())
+			.append(File.separator).append("Trinea").append(File.separator)
+			.append("AndroidDemo").append(File.separator).append("ImageCache")
+			.toString();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +55,12 @@ public class PGMainFragment extends BaseFragment {
 			Bundle savedInstanceState) {
 		LayoutInflater mInflater = LayoutInflater.from(getActivity());
 		View v = mInflater.inflate(R.layout.fragment_grid, null);
+
+		initImageUrlList();
+		IMAGE_CACHE.initData(getActivity(), TAG_CACHE);
+		IMAGE_CACHE.setContext(getActivity());
+		IMAGE_CACHE.setCacheFolder(DEFAULT_CACHE_FOLDER);
+
 		initView(v);
 		return v;
 	}
@@ -78,9 +107,12 @@ public class PGMainFragment extends BaseFragment {
 			} else {
 				holder = (ViewHolder) convertView.getTag();
 			}
-			
-			holder.row_image.setImageResource(R.drawable.ic_launcher);
-			holder.row_text.setText("test");
+
+			// holder.row_image.setImageResource(R.drawable.ic_launcher);
+
+			// get image
+			IMAGE_CACHE.get(imageUrlList.get(position), holder.row_image);
+
 			return convertView;
 		}
 
@@ -106,5 +138,131 @@ public class PGMainFragment extends BaseFragment {
 	static class ViewHolder {
 		ImageView row_image;
 		TextView row_text;
+	}
+
+	private List<String> imageUrlList;
+
+	private void initImageUrlList() {
+		imageUrlList = new ArrayList<String>();
+		imageUrlList
+				.add("http://farm8.staticflickr.com/7409/9148527822_36fa37d7ca_z.jpg");
+		imageUrlList
+				.add("http://farm4.staticflickr.com/3755/9148527824_6c156185ea.jpg");
+		imageUrlList
+				.add("http://farm8.staticflickr.com/7318/9148527808_e804baef0b.jpg");
+		imageUrlList
+				.add("http://farm8.staticflickr.com/7318/9146300275_5fe995d123.jpg");
+		imageUrlList
+				.add("http://farm8.staticflickr.com/7288/9146300469_bd3420c75b_z.jpg");
+		imageUrlList
+				.add("http://farm8.staticflickr.com/7351/9148527976_8a4e75ae87.jpg");
+		imageUrlList
+				.add("http://farm3.staticflickr.com/2888/9148527996_f05118d7de_o.jpg");
+		imageUrlList
+				.add("http://farm3.staticflickr.com/2863/9148527892_31f9377351_o.jpg");
+		imageUrlList
+				.add("http://farm8.staticflickr.com/7310/9148528008_8e8f51997a.jpg");
+	}
+
+	static {
+		/** init icon cache **/
+		OnImageCallbackListener imageCallBack = new OnImageCallbackListener() {
+
+			/**
+			 * callback function after get image successfully, run on ui thread
+			 * 
+			 * @param imageUrl
+			 *            imageUrl
+			 * @param loadedImage
+			 *            bitmap
+			 * @param view
+			 *            view need the image
+			 * @param isInCache
+			 *            whether already in cache or got realtime
+			 */
+			@Override
+			public void onGetSuccess(String imageUrl, Bitmap loadedImage,
+					View view, boolean isInCache) {
+				if (view != null && loadedImage != null) {
+					ImageView imageView = (ImageView) view;
+					imageView.setImageBitmap(loadedImage);
+					// first time show with animation
+					if (!isInCache) {
+						imageView.startAnimation(getInAlphaAnimation(2000));
+					}
+
+					// auto set height accroding to rate between height and
+					// weight
+					LayoutParams imageParams = (LayoutParams) imageView
+							.getLayoutParams();
+					imageParams.height = imageParams.width
+							* loadedImage.getHeight() / loadedImage.getWidth();
+					imageView.setScaleType(ScaleType.FIT_XY);
+				}
+			}
+
+			/**
+			 * callback function before get image, run on ui thread
+			 * 
+			 * @param imageUrl
+			 *            imageUrl
+			 * @param view
+			 *            view need the image
+			 */
+			@Override
+			public void onPreGet(String imageUrl, View view) {
+				// Log.e(TAG_CACHE, "pre get image");
+			}
+
+			/**
+			 * callback function after get image failed, run on ui thread
+			 * 
+			 * @param imageUrl
+			 *            imageUrl
+			 * @param loadedImage
+			 *            bitmap
+			 * @param view
+			 *            view need the image
+			 * @param failedReason
+			 *            failed reason for get image
+			 */
+			@Override
+			public void onGetFailed(String imageUrl, Bitmap loadedImage,
+					View view, FailedReason failedReason) {
+				Log.e(TAG_CACHE,
+						new StringBuilder(128).append("get image ")
+								.append(imageUrl)
+								.append(" error, failed type is: ")
+								.append(failedReason.getFailedType())
+								.append(", failed reason is: ")
+								.append(failedReason.getCause().getMessage())
+								.toString());
+			}
+
+			@Override
+			public void onGetNotInCache(String imageUrl, View view) {
+				if (view != null && view instanceof ImageView) {
+					((ImageView) view).setImageResource(R.drawable.ic_launcher);
+				}
+			}
+		};
+		IMAGE_CACHE.setOnImageCallbackListener(imageCallBack);
+		IMAGE_CACHE
+				.setCacheFullRemoveType(new RemoveTypeLastUsedTimeFirst<Bitmap>());
+
+		IMAGE_CACHE.setHttpReadTimeOut(10000);
+		IMAGE_CACHE.setOpenWaitingQueue(true);
+		IMAGE_CACHE.setValidTime(-1);
+		/**
+		 * close connection, default is connect keep-alive to reuse connection.
+		 * if image is from different server, you can set this
+		 */
+		// IMAGE_CACHE.setRequestProperty("Connection", "false");
+	}
+
+	public static AlphaAnimation getInAlphaAnimation(long durationMillis) {
+		AlphaAnimation inAlphaAnimation = new AlphaAnimation(0, 1);
+		inAlphaAnimation.setDuration(durationMillis);
+		return inAlphaAnimation;
 	}
 }
