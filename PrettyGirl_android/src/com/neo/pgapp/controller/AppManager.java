08b66bdd6972016ddc711fdbd02f632a162ec4e3
theoneneo <mobi.liubing@gmail.com>
2014-09-04 11:55:31 +0800

    pg

--- PrettyGirl_android/src/com/neo/pgapp/controller/AppManager.java
+++ PrettyGirl_android/src/com/neo/pgapp/controller/AppManager.java
@@ -31,7 +31,7 @@ import com.neo.pgapp.R;
  */
 public class AppManager extends BaseManager{
 	private static AppManager mInstance;
-	public static String APP_PID = "360";
+	public static String APP_PID = "baidu";
 	public int coin;
 	public static int width, height;
 	public static int curVersion;
